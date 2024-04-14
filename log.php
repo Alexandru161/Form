77e02c7e8b3e2c9939757a3bf5072aa2d6b7dc3f
Alexandru161 <65708471+Alexandru161@users.noreply.github.com>
2024-04-14 17:11:39 +0300

    log.php

---
+++ log.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Вход</title>
+<link rel="stylesheet" href="register.css">
+</head>
+<body>
+
+<div class="container">
+  <div class="form-box">
+    <form class="form" action="login.php" method="post">
+        <span class="title">Вход</span>
+        <span class="subtitle">Create a free account with your email.</span>
+        <div class="form-container">
+        <input type="text" class="input" placeholder="Login" name="login">
+        <input type="password" class="input" placeholder="Password" name="pass">
+        </div>
+        <button type="submit">Sign in</button>
+    </form>
+    <div class="form-section">
+      <p>No bitches? <a href="reg.php">регай</a> </p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
